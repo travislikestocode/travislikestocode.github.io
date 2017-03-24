6be74cb44a064d2e36771bd39aa8a1fba1e5f6f8
Test <test@ws2201-1684.local>
2017-03-24 04:02:45 -0700

    undeploy

--- whodatpokemon_states/server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect'),
-    http = require('http'),
-    port = process.env.PORT || 3000;
-
-connect()
-    .use(connect.static('public'))
-    .listen(port);
-
-console.log("Connect listening on port " + port);
